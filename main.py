f758ffa37b32e0c73f575a9ada3ddd3cc3bfeb97
rakesh160 <rakesh160@users.noreply.github.com>
2018-02-26 19:54:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def hello_name(name):
    return 'Hello %s!' % name
 
 @app.route('/hello')
-def hello_name_default(name):
+def hello_name_default():
    return 'Hello default'
 
 if __name__ == '__main__':
