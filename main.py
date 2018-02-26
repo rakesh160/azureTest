369e8479c4b02d631da6207ad18c7253127c4204
rakesh160 <rakesh160@users.noreply.github.com>
2018-02-26 20:00:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ def hello_name(name):
    hello_name_default()
    return 'Hello %s!' % name
 
-
+@app.route('/hello')
 def hello_name_default():
    return 'Hello default'
+@app.route('/helloall/<word>')
+def hello_all(name):
+   if(name != null):
+      return redirect(url_for('hello_name', name=word))
+   else:
+      return redirect(url_for('hello_name_default'))
 
 if __name__ == '__main__':
   app.run()
